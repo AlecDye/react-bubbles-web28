77a458f6ac91d8d554b2a71f31e432e2d84b7496
Alec Dye <alecdye@gmail.com>
2020-03-27 08:43:18 -0700

    Building login logic

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [state, setState] = useState({
+    credentials: {
+      username: "",
+      password: ""
+    }
+  });
+  const handleChanges = e => {
+    setState({
+      credentials: {
+        ...state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <h1>Bubble.Color</h1>
+      <p>Bringing you the bubbles with the colors</p>
+      <form>
+        <label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Lambda School"
+            value={handleChanges}
+          />
+          Username
+        </label>
+        <label>
+          <input
+            type="password"
+            name="password"
+            placeholder="i<3Lambd4"
+            value={handleChanges}
+          />
+          Password
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
